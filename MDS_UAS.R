4bad5f40253a316b02b97d1d4c7cc8c639fd3166
hhyuss <142286885+hhyuss@users.noreply.github.com>
2024-05-29 11:22:47 +0700

    Add files via upload

---
+++ MDS_UAS.R
@@ -0,0 +1,26 @@
+message('Loading Packages')
+library(rvest)
+library(tidyverse)
+library(mongolite)
+
+message('Scraping Data')
+url <- "https://www.suara.com/news/news-category/nasional#"
+page <- read_html(url)
+
+
+titles <- page %>% html_nodes(xpath = '//div[@class="text-list-item-y"]/a') %>% html_text()
+
+dates <- page %>% html_nodes(xpath = '//div[@class="date-list-item-y"]') %>% html_text()
+
+links <- page %>% html_nodes(xpath = '//div[@class="text-list-item-y"]/a') %>% html_attr("href")
+
+
+
+data <- data.frame(
+  time_scraped = Sys.time(),
+  titles = titles,
+  dates = dates,
+  links = links,
+  stringsAsFactors = FALSE
+)
+View(data)
