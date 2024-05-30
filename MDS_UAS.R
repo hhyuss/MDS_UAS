5ad656c4e8b97a79bbb3d043836dab8cad1c7437
hhyuss <142286885+hhyuss@users.noreply.github.com>
2024-05-30 15:14:51 +0700

    MDS_UAS.R

--- MDS_UAS.R
+++ MDS_UAS.R
@@ -2,25 +2,32 @@ message('Loading Packages')
 library(rvest)
 library(tidyverse)
 library(mongolite)
+library(httr)
 
 message('Scraping Data')
-url <- "https://www.suara.com/news"
+url <- "https://www.viva.co.id/"
 page <- read_html(url)
 
-
-titles <- page %>% html_nodes(xpath = '//div[@class="text-list-item-y"]/a') %>% html_text()
-
-dates <- page %>% html_nodes(xpath = '//div[@class="date-list-item-y"]') %>% html_text()
-
-links <- page %>% html_nodes(xpath = '//div[@class="text-list-item-y"]/a') %>% html_attr("href")
-
+# Extract data using XPath selectors
+judul <- page %>% html_nodes(xpath = '/html/body/div[2]/span/div[8]/div[2]/div/section[3]/div[2]/div[2]/div[2]/span/a[2]') %>% html_text()
+kategori <- page %>% html_nodes(xpath = '/html/body/div[2]/span/div[8]/div[2]/div/section[3]/div[2]/div[2]/div[2]/span/a[3]') %>% html_text()
 
 
 data <- data.frame(
   time_scraped = Sys.time(),
-  titles = titles,
-  dates = dates,
-  links = links,
+  judul = head(judul, 10),
+  kategori = head(kategori, 10),
   stringsAsFactors = FALSE
 )
+
+# MONGODB
+message('Input Data to MongoDB Atlas')
+atlas_conn <- mongo(
+  collection = Sys.getenv("ATLAS_COLLECTION"),
+  db         = Sys.getenv("ATLAS_DB"),
+  url        = Sys.getenv("ATLAS_URL")
+)
+
+atlas_conn$insert(data)
+rm(atlas_conn)
 
